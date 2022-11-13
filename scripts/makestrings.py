f737d980bb5e246bfc232bdc94a9a330f06547bf
Johannes Bader <git@bin.re>
2022-11-13 13:41:37 +0100

    new rule for Xwormmm

--- scripts/makestrings.py
+++ scripts/makestrings.py
@@ -1,18 +1,22 @@
 import argparse
+import math
 
 
 def run(path: str, prefix: str = "string", wide: bool = False):
-    nr = 0
-    with open(path, "r") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            w = " wide" if wide else ""
-            line = line.replace('"', "\\\"")
-            pattern = f'${prefix}_{nr} = "{line}"{w}'
-            print(pattern)
-            nr += 1
+    with open(path, "r") as r:
+        lines = r.readlines()
+    total = len(lines)
+    nr = 1
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        w = " wide" if wide else ""
+        line = line.replace('"', "\\\"")
+        nrf = str(nr).zfill(int(math.log10(total)+1))
+        pattern = f'${prefix}_{nrf} = "{line}"{w}'
+        print(pattern)
+        nr += 1
 
 
 if __name__ == "__main__":
